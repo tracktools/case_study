089950bbea16c98b00c77a24e791d8880a1f029b
apryet <alexandre.pryet@ensegid.fr>
2022-03-27 23:00:15 +0200

    new pp set and refinement zones

--- ml_setup.py
+++ ml_setup.py
@@ -14,8 +14,8 @@ from tracktools import ParticleGenerator
 # ----- General settings ----                                                        
 #-----------------------------------#
 
-case_ids = [i for i in range(1,11)] # see surveys.xlsx
-#case_ids = [2]
+#case_ids = [i for i in range(1,11)] # see surveys.xlsx
+case_ids = [2]
 
 mf6_exe = 'mf6'
 mp7_exe = 'mp7'
